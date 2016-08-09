ca916c16c036335a42c8cf7df46210f81e83e0a5
Nidhin Surendran <nidhin.surendran@thomsonreuters.com>
2016-08-09 15:52:49 -0400

    registration step definition done

--- features/step_definitions/todoist-signup-steps.js
+++ features/step_definitions/todoist-signup-steps.js
@@ -19,8 +19,23 @@ module.exports = function() {
   this.Then(/^Signup page should be shown with options to signup with Google and Email$/, function () {
     this.driver.switchTo().frame(this.driver.findElement(this.By.name('GB_frame')));
     this.driver.switchTo().frame(this.driver.findElement(this.By.id('GB_frame')));
-    
+
     return this.driver.findElements({ id: 'submit_button' })
+      .then(function(elements) {
+        expect(elements.length).to.not.equal(0);
+      });
+  });
+
+  this.When(/^User enters details and click signup$/, function(){
+    this.driver.findElement({ id: 'full_name' }).sendKeys('Nidhins');
+    this.driver.findElement({ id: 'email' }).sendKeys('Nidhins' + Math.floor((Math.random() * 1000) + 1) + '@ypmail.com');
+    this.driver.findElement({ id: 'password' }).sendKeys('Welcome1');
+    return this.driver.findElement({ id: 'submit_button' })
+      .click();
+  });
+
+  this.Then(/^Signup success message should be shown$/, function(){
+    return this.driver.findElements({ xpath: '//h1[contains(text(),"Hi, Nidhin")]' })
       .then(function(elements) {
         expect(elements.length).to.not.equal(0);
       });
